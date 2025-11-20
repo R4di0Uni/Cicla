b0c4230e19c4f4113476e15914bd21cfb80962f5
Daniel Almeida <115008409+R4di0Uni@users.noreply.github.com>
2025-11-20 19:19:53 +0000

    Update ESP32.ino

--- src/iot/ESP32/ESP32.ino
+++ src/iot/ESP32/ESP32.ino
@@ -1,20 +1,36 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
+#include "HX711.h"
+#include <MPU6050.h>
 
 const char* ssid = "YourWiFi";
 const char* password = "YourPass";
-
-const char* mqtt_server = "192.168.x.x";   
+const char* mqtt_server = "192.168.x.x";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+#define BUZZER_PIN 16
+#define REED_PIN   17
+
+#define HX_SCK 15
+#define HX_DT  2
+
+#define SDA_PIN 27
+#define SCL_PIN 14
+
+HX711 scale;
+MPU6050 mpu;
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Received cmd: ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
+  for (int i = 0; i < length; i++) Serial.print((char)payload[i]);
   Serial.println();
+
+  if (String((char*)payload).startsWith("beep")) {
+    tone(BUZZER_PIN, 1000, 300);
+  }
 }
 
 void reconnect() {
@@ -26,7 +42,7 @@ void reconnect() {
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 2 seconds");
+      Serial.println(" retrying in 2 seconds");
       delay(2000);
     }
   }
@@ -34,8 +50,12 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
 
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(REED_PIN, INPUT_PULLUP);
+
+  
+  WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -45,14 +65,42 @@ void setup() {
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  scale.begin(HX_DT, HX_SCK);
+  scale.set_scale();  
+  scale.tare();      
+  Serial.println("HX711 initialized");
+
+  Wire.begin(SDA_PIN, SCL_PIN, 400000);
+  mpu.initialize();
+
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 NOT connected!");
+  } else {
+    Serial.println("MPU6050 connected");
+  }
 }
 
 void loop() {
   if (!client.connected()) reconnect();
   client.loop();
 
-  // Publish temperature example
-  client.publish("esp32/temp", "25.3");
+  int reedState = digitalRead(REED_PIN); 
 
-  delay(2000);
+  float weight = scale.get_units(10);
+
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  char payload[200];
+  sprintf(payload,
+    "{\"reed\":%d, \"weight\":%.2f, \"ax\":%d, \"ay\":%d, \"az\":%d, \"gx\":%d, \"gy\":%d, \"gz\":%d}",
+    reedState, weight, ax, ay, az, gx, gy, gz
+  );
+
+  client.publish("esp32/sensors", payload);
+
+  Serial.println(payload);
+
+  delay(500);  
 }
